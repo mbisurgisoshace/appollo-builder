43c3c2c8773cfc586eae38976f96f7a1262cd814
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2026-03-06 01:03:14 -0300

    Fix build issues

--- src/components/rich-text-editor/plugins/picker/columns-layout-picker-plugin.tsx
+++ src/components/rich-text-editor/plugins/picker/columns-layout-picker-plugin.tsx
@@ -4,12 +4,13 @@ import { Columns3Icon } from "lucide-react";
 import { ComponentPickerOption } from "@/components/rich-text-editor/plugins/picker/component-picker-option";
 
 export function ColumnsLayoutPickerPlugin() {
-  return new ComponentPickerOption("Columns Layout", {
-    icon: <Columns3Icon className="size-4" />,
-    keywords: ["columns", "layout", "grid"],
-    onSelect: (_, editor, showModal) =>
-      showModal("Insert Columns Layout", (onClose) => (
-        //<InsertLayoutDialog activeEditor={editor} onClose={onClose} />
-      )),
-  });
+  return null;
+  // return new ComponentPickerOption("Columns Layout", {
+  //   icon: <Columns3Icon className="size-4" />,
+  //   keywords: ["columns", "layout", "grid"],
+  //   onSelect: (_, editor, showModal) =>
+  //     showModal("Insert Columns Layout", (onClose) => (
+  //       // <InsertLayoutDialog activeEditor={editor} onClose={onClose} />
+  //     )),
+  // });
 }
